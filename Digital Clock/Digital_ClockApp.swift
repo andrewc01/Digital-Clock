2f55d6b89d9356d0068dcbb62e59d9153f59f74f
andy <jhyeonc01@gmail.com>
2023-01-21 16:09:13 +0900

    Initial Commit

---
+++ Digital Clock/Digital_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_ClockApp.swift
+//  Digital Clock
+//
+//  Created by Andy C on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Digital_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
