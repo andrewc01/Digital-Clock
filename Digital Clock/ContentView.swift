ed2ac9777badc5858b9ea05b149530cef14a3f8a
andy <jhyeonc01@gmail.com>
2023-01-21 16:28:15 +0900

    version 1.0 of digital clock made with swiftUI

--- Digital Clock/ContentView.swift
+++ Digital Clock/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var date = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("\(timeString(date: date))")
+                    .onAppear(perform: {let _ = self.updateTimer})
+                    .font(.system(size: 80))
+                    .foregroundColor(.white)
+            }
+            .padding()
         }
-        .padding()
+        
+    }
+    
+    var timeFormat: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E, MMM d, yyyy hh:mm:ss a"
+        return formatter
+    }
+    
+    func timeString(date: Date) -> String {
+        let time = timeFormat.string(from: date)
+        return time
+    }
+    
+    var updateTimer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1,
+                             repeats: true,
+                             block: {_ in self.date = Date()
+        })
     }
 }
 
